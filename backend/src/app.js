0c88b64128820625089b6a0dfeb6196edf871de0
sanfarish <farisfalah@gmail.com>
2024-12-19 22:46:59 +0700

    insert essential dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import compression from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+import logger from 'morgan'
 const app = express()
+
+app.use(logger('dev'))
+app.use(helmet())
+app.use(cors({ origin: process.env.CORS_ORIGIN }))
+app.use(compression())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
